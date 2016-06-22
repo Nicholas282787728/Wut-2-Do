20d2bfccc6d9b0f0caea3797f8e3e5ce739d9fb9
Zhiyuan-Amos <zhiyuan_1994@yahoo.com.sg>
2016-06-22 13:31:08 +0800

    Add files via upload

---
+++ database_main/db_online.php
@@ -0,0 +1,8 @@
+<?php	
+	$connection = mysqli_connect('mysql11.000webhost.com', 'a3710120_zhiyuan', 'amos0907', 'a3710120_wut2do');
+	//host, username, password, database
+
+	if(!$connection){
+		die("Database connection failed");
+	}
+?>
